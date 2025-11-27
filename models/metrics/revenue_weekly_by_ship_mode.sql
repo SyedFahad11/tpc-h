45fb4af62514ab2fbfbb631c4154759905761501
Captain Suman <syedhu987@gmail.com>
2025-11-27 15:06:27 +0000

    Fixed: metricflow_time_spine, revenue_weekly_by_ship_mode

--- models/metrics/revenue_weekly_by_ship_mode.sql
+++ models/metrics/revenue_weekly_by_ship_mode.sql
@@ -2,7 +2,13 @@
 -- models/metrics/revenue_weekly_by_ship_mode.sql
 {{ config(materialized='table') }}
 
-{{ dbt_metric_utils_materialize(
-    metrics=['total_revenue'],
-    group_by=['metric_time__week', 'ship_mode']
-) }}
+with src as (
+    select order_date, ship_mode, gross_item_sales_amount
+    from {{ ref('fct_order_items') }}
+)
+select
+    date_trunc('week', order_date) as metric_time__week,
+    ship_mode,
+    sum(gross_item_sales_amount) as total_revenue
+from src
+group by 1, 2
--- models/semantic/metricflow_time_spine.sql
+++ models/semantic/metricflow_time_spine.sql
@@ -1,12 +1,13 @@
-
 with spine as (
 
-  {{ dbt_utils.date_spine(
-      datepart="day",
-      start_date="to_date('2000-01-01')",
-      end_date="current_date()"
-  ) }}
+    {{ dbt_utils.date_spine(
+        datepart="day",
+        start_date="to_date('2000-01-01')",
+        end_date="current_date()"
+    ) }}
 
 )
 
-select cast(date_spine as date) as date_day
+select 
+    cast(date_day as date) as date_day
+from spine
